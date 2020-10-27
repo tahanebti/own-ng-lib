69020664b5515e8e120991978d61bf08dd27413c
tahanebti <taha.nebti@gmail.com>
2020-10-27 14:05:03 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sadarly-front';
+}
